31c8b98d82524faf5bf6911890e4424e3d5e45a4
Harinder Dulai <haridulaix@gmail.com>
2023-02-17 07:57:45 +0000

    Set input with form submit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
-  const addTodo = e => {
+  const addTodo = (e) => {
+    e.preventDefault();
     setTodos([...todos, input]);
-    setInput('');
-  }
+    setInput("");
+  };
 
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <input value={input} onChange={e => setInput(e.target.value)} type="text" />
-      <button onClick={addTodo}>Add todo</button>
-
+      <form>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+        />
+        <button type="submit" onClick={addTodo}>
+          Add todo
+        </button>
+      </form>
       <h2>List of Todos</h2>
       {todos.map((todo) => (
         <p>{todo}</p>
